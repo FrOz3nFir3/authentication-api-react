7d2056ee725c49ead371239c4acde908a32df448
Kush Surti <kushsurti05@gmail.com>
2023-01-21 13:31:43 +0530

    back to browser router for github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Routes, Route, HashRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter} from "react-router-dom";
 
 import App from "./App";
 import {Profile} from "./Profile";
@@ -10,13 +10,13 @@ import {Login} from "./Login";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/authentication-api-react" element={<App/>}>
           <Route path="profile" element={<Profile/>}/>
           <Route path="login" element={<Login/>}/>
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
